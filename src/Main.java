e0d731252f6ace27f172ec669d0b17945a736915
Aisha <aisha.orozbekova@alatoo.edu.kg>
2024-10-08 17:25:25 +0600

    methods were added. code was improved

--- src/Main.java
+++ src/Main.java
@@ -7,29 +7,16 @@ public class Main {
         Scanner sc = new Scanner(System.in);
         int[] myArray = new int[7];
 
-        int indexCount = 0;
-        int index;
+        shuffleBoxes(myArray, rand);
 
-        while (indexCount < 3) {
-            index = rand.nextInt(7);
-            if (myArray[index] == 0) {
-                myArray[index] = 1;
-                indexCount++;
-            }
-        }
-
-        for(int i=0; i<myArray.length; i++) {
-            System.out.print(myArray[i] + " ");
-        }
-
-        int numberOfTries = 0;
-        int maxNumberOfTries = 5;
+        int numOfTries = 0;
+        int maxNumOfTries = 5;
 
         boolean gameWon = false;
 
         while (!gameWon) {
-            while (numberOfTries < maxNumberOfTries) {
-                System.out.println("Enter three numbers between 1 and 7 where you think the boxes might be located");
+            while (numOfTries < maxNumOfTries) {
+                System.out.println("\nEnter three numbers between 1 and 7 where you think the boxes might be located");
                 int suggestion1 = sc.nextInt() - 1;
                 int suggestion2 = sc.nextInt() - 1;
                 int suggestion3 = sc.nextInt() - 1;
@@ -37,36 +24,64 @@ public class Main {
                 int correctGuesses = (myArray[suggestion1] == 1 ? 1:0) +
                                      (myArray[suggestion2] == 1 ? 1:0) +
                                      (myArray[suggestion3] == 1 ? 1:0);
+
                 if (correctGuesses == 1) {
                     System.out.println("You found 1 box");
+                    showTries(maxNumOfTries, numOfTries);
                 } else if (correctGuesses == 2) {
                     System.out.println("You found 2 boxes");
+                    showTries(maxNumOfTries, numOfTries);
                 } else if (correctGuesses == 3) {
                     System.out.println("You found 3 boxes. Congratulations!");
                     gameWon = true;
                     break;
                 } else{
                     System.out.println("You found nothing");
+                    showTries(maxNumOfTries, numOfTries);
                 }
-
-                numberOfTries++;
+                numOfTries++;
             }
-            if (gameWon == false && maxNumberOfTries == numberOfTries) {
-                System.out.println("You have exceeded the maximum number of attempts, the boxes are shuffled again. ");
-                while (indexCount < 3) {
-                    indexCount = 0;
-                    index = rand.nextInt(7);
-                    if (myArray[index] == 0) {
-                        myArray[index] = 1;
-                        indexCount++;
-                    }
-                }
 
-                for(int i=0; i<myArray.length; i++) {
-                    System.out.print(myArray[i] + " ");
-                }
-                numberOfTries = 0;
+            if (maxNumOfTries == numOfTries) {
+                int i = 0;
+                showBoxes(myArray);
+                System.out.println("\nYou have exceeded the maximum number of attempts, the boxes are shuffled again. ");
+                shuffleBoxes(myArray, rand);
+                numOfTries = 0;
             }
         }
     }
+
+    public static void shuffleBoxes(int [] myArray, Random rand) {
+            int indexCount = 0;
+            int index;
+            int k = 0;
+
+            for(;k<7;k+=1){
+                myArray[k] = 0;
+            }
+
+            while (indexCount < 3) {
+                index = rand.nextInt(7);
+                if (myArray[index] == 0) {
+                    myArray[index] = 1;
+                    indexCount++;
+                }
+            }
+    }
+
+    public static void showBoxes(int [] myArray) {
+        int i = 0;
+        System.out.println("\nThat's where the boxes were hidden:");
+        for(; i<myArray.length; i+=1) {
+            System.out.print(myArray[i] + " ");
+        }
+    }
+
+    public static void showTries(int maxNumOfTries, int numOfTries) {
+        String result = (maxNumOfTries-numOfTries-1) == 1 ? "attempt" : "attemps";
+        System.out.println("You have " + (maxNumOfTries-numOfTries-1) + " " + result + " left.");
+    }
+
+
 }
